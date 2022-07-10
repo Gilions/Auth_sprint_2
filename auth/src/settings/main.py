655f0132fd5a408b48712d7693508552bcaf4d1d
vitalijsofronuk <v.sofronyuk@puls.ru>
2022-07-10 23:43:04 +0300

    feat: Added jaeger

--- auth/src/settings/main.py
+++ auth/src/settings/main.py
@@ -1,6 +1,8 @@
 from api import init_api
-from flask import Flask
+from flask import Flask, request
 from flask_marshmallow import Marshmallow
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
 from settings.config import configuration
 from settings.database import init_db
 from settings.datastore import init_datastore, init_datastore_commands
@@ -9,6 +11,15 @@ from settings.inc_rate_limitter import init_rate_limiter
 
 
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    request_id = request.headers.get('X-Request-Id')
+    if not request_id:
+        raise RuntimeError('request id is required')
+
+
 app.config.from_object(configuration)
 
 init_db(app)
@@ -17,6 +28,7 @@ init_datastore_commands(app)
 init_api(app)
 init_jwt(app)
 init_rate_limiter(app)
+FlaskInstrumentor().instrument_app(app)
 
 ma = Marshmallow(app)
 
